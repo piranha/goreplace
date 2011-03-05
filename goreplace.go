c317483fa13fe59bcba1f7a9f85a4431bb5c4cba
Alexander Solovyov <alexander@solovyov.net>
2011-03-05 16:57:48 +0100

    file exclusion by mask

--- goreplace.go
+++ goreplace.go
@@ -1,17 +1,32 @@
 package main
 
 import (
-	goopt "github.com/droundy/goopt"
 	"os"
 	"path"
 	"fmt"
 	"regexp"
 	"bytes"
+	goopt "github.com/droundy/goopt"
 	"./highlight"
 )
 
-var Pattern *regexp.Regexp
 var byteNewLine []byte = []byte("\n")
+// Used to prevent appear of sparse newline at the end of output
+var prependNewLine = false
+
+type StringList []string
+var IgnoreDirs = StringList{"autom4te.cache", "blib", "_build", ".bzr", ".cdv",
+	"cover_db", "CVS", "_darcs", "~.dep", "~.dot", ".git", ".hg", "~.nib",
+    ".pc", "~.plst", "RCS", "SCCS", "_sgbak", ".svn"}
+
+type RegexpList []*regexp.Regexp
+var IgnoreFiles = regexpList([]string{`~$`, `#.+#$`, `[._].*\.swp$`, `core\.[0-9]+$`,
+	`\.pyc$`, `\.o$`, `\.6$`})
+
+var onlyName = goopt.Flag([]string{"-n", "--filename"}, []string{},
+	"print only filenames", "")
+var ignoreFiles = goopt.Strings([]string{"-x", "--exclude"}, "RE",
+	"exclude files that match the regexp from search")
 
 func main() {
 	goopt.Description = func() string {
@@ -25,11 +40,12 @@ func main() {
 		return
 	}
 
-	var err os.Error
-	Pattern, err = regexp.Compile(goopt.Args[0])
+	IgnoreFiles = append(IgnoreFiles, regexpList(*ignoreFiles)...)
+
+	pattern, err := regexp.Compile(goopt.Args[0])
 	errhandle(err, "can't compile regexp %s", goopt.Args[0])
 
-	searchFiles()
+	searchFiles(pattern)
 }
 
 func errhandle(err os.Error, moreinfo string, a ...interface{}) {
@@ -41,18 +57,46 @@ func errhandle(err os.Error, moreinfo string, a ...interface{}) {
 	os.Exit(1)
 }
 
-type Visitor struct{}
+func regexpList(sa []string) RegexpList {
+	ra := make(RegexpList, len(sa))
+	for i, s := range sa {
+		ra[i] = regexp.MustCompile(s)
+	}
+	return ra
+}
 
-func (v *Visitor) VisitDir(p string, fi *os.FileInfo) bool {
-	if fi.Name == ".hg" {
+func searchFiles(pattern *regexp.Regexp) {
+	v := &GRVisitor{pattern}
+
+	errors := make(chan os.Error, 64)
+
+	path.Walk(".", v, errors)
+
+	select {
+	case err := <-errors:
+		errhandle(err, "some error")
+	default:
+	}
+}
+
+type GRVisitor struct{
+	pattern *regexp.Regexp
+}
+
+func (v *GRVisitor) VisitDir(fn string, fi *os.FileInfo) bool {
+	if IgnoreDirs.Contains(fi.Name) {
 		return false
 	}
 	return true
 }
 
-func (v *Visitor) VisitFile(p string, fi *os.FileInfo) {
+func (v *GRVisitor) VisitFile(fn string, fi *os.FileInfo) {
+	if IgnoreFiles.Match(fn) {
+		return
+	}
+
 	if fi.Size >= 1024*1024*10 {
-		fmt.Fprintf(os.Stderr, "Skipping %s, too big: %d\n", p, fi.Size)
+		fmt.Fprintf(os.Stderr, "Skipping %s, too big: %d\n", fn, fi.Size)
 		return
 	}
 
@@ -60,23 +104,23 @@ func (v *Visitor) VisitFile(p string, fi *os.FileInfo) {
 		return
 	}
 
-	f, err := os.Open(p, os.O_RDONLY, 0666)
-	errhandle(err, "can't open file %s", p)
+	f, err := os.Open(fn, os.O_RDONLY, 0666)
+	errhandle(err, "can't open file %s", fn)
 
 	content := make([]byte, fi.Size)
 	n, err := f.Read(content)
-	errhandle(err, "can't read file %s", p)
+	errhandle(err, "can't read file %s", fn)
 	if int64(n) != fi.Size {
 		panic(fmt.Sprintf("Not whole file was read, only %d from %d",
 			n, fi.Size))
 	}
 
-	searchFile(p, content)
+	v.SearchFile(fn, content)
 
 	f.Close()
 }
 
-func searchFile(p string, content []byte) {
+func (v *GRVisitor) SearchFile(p string, content []byte) {
 	linenum := 1
 	last := 0
 	hadOutput := false
@@ -86,16 +130,24 @@ func searchFile(p string, content []byte) {
 		binary = true
 	}
 
-	for _, bounds := range Pattern.FindAllIndex(content, -1) {
-		if binary {
-			fmt.Printf("Binary file %s matches\n", p)
-			hadOutput = true
-			break
+	for _, bounds := range v.pattern.FindAllIndex(content, -1) {
+		if prependNewLine {
+			fmt.Println("")
+			prependNewLine = false
 		}
 
 		if !hadOutput {
-			highlight.Printf("green", "%s\n", p)
 			hadOutput = true
+			if binary && !*onlyName{
+				fmt.Printf("Binary file %s matches\n", p)
+				break
+			} else {
+				highlight.Printf("green", "%s\n", p)
+			}
+		}
+
+		if *onlyName {
+			return
 		}
 
 		linenum += bytes.Count(content[last:bounds[0]], byteNewLine)
@@ -107,11 +159,11 @@ func searchFile(p string, content []byte) {
 		}
 
 		highlight.Printf("bold yellow", "%d:", linenum)
-		highlight.Reprintf("on_yellow", Pattern, "%s\n", content[begin:end])
+		highlight.Reprintf("on_yellow", v.pattern, "%s\n", content[begin:end])
 	}
 
 	if hadOutput {
-		fmt.Println("")
+		prependNewLine = true
 	}
 }
 
@@ -139,16 +191,20 @@ func beginend(s []byte, start int, finish int) (begin int, end int) {
 	return
 }
 
-func searchFiles() {
-	v := &Visitor{}
-
-	errors := make(chan os.Error, 64)
-
-	path.Walk(".", v, errors)
-
-	select {
-	case err := <-errors:
-		errhandle(err, "some error")
-	default:
+func (sl StringList) Contains(s string) bool {
+	for _, x := range sl {
+		if x == s {
+			return true
+		}
 	}
+	return false
+}
+
+func (rl RegexpList) Match(s string) bool {
+	for _, x := range rl {
+		if x.Match([]byte(s)) {
+			return true
+		}
+	}
+	return false
 }
