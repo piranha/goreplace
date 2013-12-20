38584841541fc58c59f388755544f273a335b16c
Alexander Solovyov <alexander@solovyov.net>
2013-12-20 13:08:02 +0200

    improve help strings and their order

--- goreplace.go
+++ goreplace.go
@@ -21,20 +21,20 @@ const (
 var byteNewLine = []byte("\n")
 
 var opts struct {
+	Replace         *string  `short:"r" long:"replace" description:"replace found substrings with RE" value-name:"RE"`
+	Force           bool     `short:""  long:"force" description:"force replacement in binary files"`
 	IgnoreCase      bool     `short:"i" long:"ignore-case" description:"ignore pattern case"`
-	OnlyName        bool     `short:"n" long:"filename" description:"print only filenames"`
-	FindFiles       bool     `short:"f" long:"find-files" description:"search for files and not for text in them"`
-	IgnoreFiles     []string `short:"x" long:"exclude" description:"exclude files that match the regexp from search" value-name:"RE"`
-	AcceptFiles     []string `short:"o" long:"only" description:"search only in files that match the regexp" value-name:"RE"`
-	SingleLine      bool     `short:"s" long:"singleline" description:"match on a single line (^/$ will be begginning/end of line)"`
-	PlainText       bool     `short:"p" long:"plain" description:"search plain text"`
-	Replace         *string  `short:"r" long:"replace" description:"replace found substrings with this string"`
+	SingleLine      bool     `short:"s" long:"singleline" description:"match single line (^/$ will match beginning/end of line)"`
+	PlainText       bool     `short:"p" long:"plain" description:"treat pattern as plain text"`
+	IgnoreFiles     []string `short:"x" long:"exclude" description:"exclude files with names that match regexp RE from search" value-name:"RE"`
+	AcceptFiles     []string `short:"o" long:"only" description:"search only in files with names that match regexp RE" value-name:"RE"`
 	NoGlobalIgnores bool     `short:"I" long:"no-autoignore" description:"do not read .git/.hgignore files"`
-	Force           bool     `long:"force" description:"force replacement in binary files"`
+	FindFiles       bool     `short:"f" long:"find-files" description:"search in file names"`
+	OnlyName        bool     `short:"n" long:"filename" description:"print only filenames"`
 	Verbose         bool     `short:"v" long:"verbose" description:"be verbose (show non-fatal errors, like unreadable files)"`
-	ShowVersion     bool     `short:"V" long:"version" description:"show version and exit"`
-	ShowHelp        bool     `long:"help" description:"show this help message"`
 	NoColors        bool     `short:"c" long:"no-colors" description:"do not show colors in output"`
+	ShowVersion     bool     `short:"V" long:"version" description:"show version and exit"`
+	ShowHelp        bool     `short:"h" long:"help" description:"show this help message"`
 }
 
 func main() {
